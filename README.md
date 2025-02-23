3101138ec664c3a4bdd224f6e1d6594bac0e508d
Cairo <cairodealmeida@gmail.com>
2025-02-23 20:31:58 -0300

    adicionar scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um repositorio para treinar programação, clonando o http://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Louise, Eu te amo muito mesmo! S2 S2 </h1>
+  return <h1>Louise, Eu te amo muito mesmo! S2 S2 </h1>;
 }
 
 export default Home;
